aa573ef5884cb0e1dcacdcc6484423a7bc842f78
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-05-24 20:32:57 +0100

    Abdelmawla: Exercices: Readline (prompt)

--- prompt.c
+++ prompt.c
@@ -17,13 +17,13 @@ char **_splitstr(char *str)
 	char *token, **arr_tokens = NULL, delim[] = " \t\n";
 	int token_count = 0, capacity = 0, i;
 
-	token = strtok(str,delim);
+	token = strtok(str, delim);
 	while (token != NULL)
 	{
 		if (token_count >= capacity)
 		{
 			capacity += INCRE_CAPACITY;
-			arr_tokens = realloc(arr_tokens, capacity * sizeof(char*));
+			arr_tokens = realloc(arr_tokens, capacity * sizeof(char *));
 			if (arr_tokens == NULL)
 			{
 				printf("Memory allocation error.\n");
@@ -31,7 +31,7 @@ char **_splitstr(char *str)
 			}
 		}
 		arr_tokens[token_count] = malloc(sizeof(char) * (strlen(token) + 1));
-	       	if (arr_tokens[token_count] == NULL)
+		if (arr_tokens[token_count] == NULL)
 		{
 			printf("Memory Allocation error.\n");
 			exit(EXIT_FAILURE);
